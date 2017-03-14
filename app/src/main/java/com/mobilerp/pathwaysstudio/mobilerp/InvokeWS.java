743af0f49f3cc16a062e44e6ff719dfebf717323
Eligio Becerra <eligio_bz@mail.com>
2017-03-13 19:39:39 -0600

    Added license to files

--- app/src/main/java/com/mobilerp/pathwaysstudio/mobilerp/InvokeWS.java
+++ app/src/main/java/com/mobilerp/pathwaysstudio/mobilerp/InvokeWS.java
@@ -1,3 +1,21 @@
+/**
+ * Created by mloki on 01/03/2017.
+ * Copyright (C) 2017  mloki
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.mobilerp.pathwaysstudio.mobilerp;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -10,10 +28,6 @@ import java.io.UnsupportedEncodingException;
 
 import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.entity.StringEntity;
-
-/**
- * Created by mloki on 01/03/2017.
- */
 
 /**
  * This class invokes all the call in the WebService
--- app/src/main/java/com/mobilerp/pathwaysstudio/mobilerp/RequestResult.java
+++ app/src/main/java/com/mobilerp/pathwaysstudio/mobilerp/RequestResult.java
@@ -1,16 +1,44 @@
+/**
+ * Created by mloki on 01/03/2017.
+ * Copyright (C) 2017  mloki
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.mobilerp.pathwaysstudio.mobilerp;
 
 import org.json.JSONObject;
-
-/**
- * Created by mloki on 01/03/2017.
- */
 
 public class RequestResult {
     private JSONObject jsonResult;
     private String stringResult;
     private int codeResult;
 
+
+    // Init with actual objects
+    public RequestResult(JSONObject jo, String st, int cr) {
+        jsonResult = jo;
+        stringResult = st;
+        codeResult = cr;
+    }
+
+    // Init everything as null
+    public RequestResult() {
+        jsonResult = null;
+        stringResult = null;
+        codeResult = -1;
+    }
 
     public JSONObject getJsonResult() {
         return jsonResult;
@@ -34,20 +62,6 @@ public class RequestResult {
 
     public void setCodeResult(int codeResult) {
         this.codeResult = codeResult;
-    }
-
-    // Init with actual objects
-    public RequestResult(JSONObject jo, String st, int cr) {
-        jsonResult = jo;
-        stringResult = st;
-        codeResult = cr;
-    }
-
-    // Init everything as null
-    public RequestResult() {
-        jsonResult = null;
-        stringResult = null;
-        codeResult = -1;
     }
 
 }

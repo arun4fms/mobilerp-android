218047fb8e30c6ec91a55adf461539a421c9f99e
Eligio Becerra <eligio_bz@mail.com>
2017-07-07 13:03:38 -0500

    Working on MainActivity to have a proper usage flow.

--- app/src/main/java/com/mobilerp/pathwaysstudio/mobilerp/MainActivity.java
+++ app/src/main/java/com/mobilerp/pathwaysstudio/mobilerp/MainActivity.java
@@ -13,13 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Hub extends AppCompatActivity
+public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hub);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -46,7 +46,7 @@ public class Hub extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.hub, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -71,13 +71,9 @@ public class Hub extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_sales) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_manager) {
 
         } else if (id == R.id.nav_share) {
 
